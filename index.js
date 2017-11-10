0a330db7a54544caca611ec9728f1a878109311b
Dave Kokandy <drkokandy@gmail.com>
2017-11-10 07:01:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const driver = 'bob';
 
-function callCode() {
-  if (driver === 'bob') {
-    console.log('this code was called')
-    console.log(`driver`)
-  }
-}
+console.log('this code was called')
+console.log(`driver`)
+console.log(`driver`, 'is the driver variable name')
